f679f548d0275f01cf1c758c126820b0f38dd3f9
George Mocioiu <93079924+geogabi@users.noreply.github.com>
2023-12-04 15:36:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from app.initialize import create_app
 
 app = create_app()
+app.app_context().push()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+    # app.run(debug=True, use_reloader=False)
